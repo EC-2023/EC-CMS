f6c0d20f3c407288ffd3466a5b4da77cfb107a2b
lulueve3 <89736351+lulueve3@users.noreply.github.com>
2023-05-03 16:30:48 +0700

    Thêm địa chỉ trong checkout

--- src/helpers/other.js
+++ src/helpers/other.js
@@ -15,4 +15,39 @@ export const fetchProvinces = async () => {
   export const fetchWards = async (districtCode) => {
 	let data = await Object.values(wards);
 	return data.filter((item) => item.parent_code === districtCode);
+  
+  };
+  export const getProvinceCodeFromName = async (provinceName) => {
+    const provinces = await fetchProvinces();
+    const province = provinces.find((province) => province.name === provinceName);
+    return province ? province.code : null;
+  };
+  
+  export const getDistrictCodeFromName = async (districtName) => {
+    const districts = await fetchDistricts();
+    const district = districts.find((district) => district.name === districtName);
+    return district ? district.code : null;
+  };
+  
+  export const getWardCodeFromName = async (wardName) => {
+    const wards = await fetchWards();
+    const ward = wards.find((ward) => ward.name === wardName);
+    return ward ? ward.code : null;
+  };
+  export const getProvinceNameFromCode = async (provinceCode) => {
+    const provinces = await fetchProvinces();
+    const province = provinces.find((province) => province.code === provinceCode);
+    return province ? province.name : null;
+  };
+  
+  export const getDistrictNameFromCode = async (districtCode) => {
+    const districts = await fetchDistricts();
+    const district = districts.find((district) => district.code === districtCode);
+    return district ? district.name : null;
+  };
+  
+  export const getWardNameFromCode = async (wardCode) => {
+    const wards = await fetchWards();
+    const ward = wards.find((ward) => ward.code === wardCode);
+    return ward ? ward.name : null;
   };
--- src/pages/other/Checkout.js
+++ src/pages/other/Checkout.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getDiscountPrice } from "../../helpers/product";
@@ -6,16 +6,104 @@ import SEO from "../../components/seo";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import UserAddressAPI from "../../api/UserAddressAPI";
+import {
+  fetchProvinces,
+  fetchDistricts,
+  fetchWards,
+  getDistrictCodeFromName,
+  getProvinceCodeFromName,
+  getWardCodeFromName,
+  getDistrictNameFromCode,
+  getProvinceNameFromCode,
+  getWardNameFromCode,
+} from "../../helpers/other";
 
 const Checkout = () => {
+  const [provinces, setProvinces] = useState([]);
+  const [districts, setDistricts] = useState([]);
+  const [wards, setWards] = useState([]);
+
+  const [selectedProvince, setSelectedProvince] = useState("");
+  const [selectedDistrict, setSelectedDistrict] = useState("");
+  const [selectedWard, setSelectedWard] = useState("");
+
+  useEffect(() => {
+    // Fetch provinces data and set it to provinces state
+    // This should be replaced with your actual API call
+    fetchProvinces().then((data) => setProvinces(data));
+  }, []);
+
+  useEffect(() => {
+    if (selectedProvince) {
+      console.log("lấy distric");
+      // Fetch districts based on the selected province
+      // Replace with your actual API call
+      fetchDistricts(selectedProvince).then((data) => {
+        setDistricts(data.slice(1));
+        console.log(data);
+      });
+    }
+  }, [selectedProvince]);
+
+  useEffect(() => {
+    if (selectedDistrict) {
+      // Fetch wards based on the selected district
+      // Replace with your actual API call
+      fetchWards(selectedDistrict).then((data) => setWards(data));
+    }
+  }, [selectedDistrict]);
+
+  const handleProvinceChange = (e) => {
+    setSelectedProvince(e.target.value);
+    setSelectedDistrict("");
+    setSelectedWard("");
+    setSelectedAddress({
+      ...selectedAddress,
+      city: e.target.selectedOptions[0].innerText,
+    });
+  };
+
+  const handleDistrictChange = (e) => {
+    setSelectedDistrict(e.target.value);
+    setSelectedWard("");
+    setSelectedAddress({
+      ...selectedAddress,
+      district: e.target.selectedOptions[0].innerText,
+    });
+  };
+
+  const handleWardChange = (e) => {
+    setSelectedWard(e.target.value);
+    setSelectedAddress({
+      ...selectedAddress,
+      ward: e.target.selectedOptions[0].innerText,
+    });
+  };
+
+  useEffect(() => {
+    const getUserAddressList = async () => {
+      try {
+        const response = await UserAddressAPI.getMyUserAddressList();
+        console.log(response.data);
+        setAddressUser(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("faild", error);
+        setIsLoading(false);
+      }
+    };
+    getUserAddressList();
+  }, []);
+
+  const handleChangeState = (key, value) => {
+    setSelectedAddress({ ...selectedAddress, [key]: value });
+  };
+
+  const [addressUser, setAddressUser] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const [selectedAddress, setSelectedAddress] = useState(null);
-const addresses = [
-  { id: 1, name: 'Địa chỉ 1', address: '123 Đường A, Quận 1, TP.HCM' },
-  { id: 2, name: 'Địa chỉ 2', address: '456 Đường B, Quận 2, TP.HCM' },
-  { id: 3, name: 'Địa chỉ 3', address: '789 Đường C, Quận 3, TP.HCM' },
-];
+
   let cartTotalPrice = 0;
 
   let { pathname } = useLocation();
@@ -52,111 +140,122 @@ const addresses = [
               <div className="row">
                 <div className="col-lg-7">
                   <div className="billing-info-wrap">
-                    <h3>Billing Details</h3>
-                    <select
-                      value={selectedAddress?.id}
-                      onChange={(event) => {
-                        const selectedId = parseInt(event.target.value);
-                        const selectedAddress = addresses.find(
-                          (address) => address.id === selectedId
-                        );
-                        setSelectedAddress(selectedAddress);
-                      }}
-                    >
+                    <h3>Địa chỉ chi tiết</h3>
+                      <select
+                        value={addressUser?.Id}
+                        onChange={(event) => {
+                          const selectedId = event.target.value;
+                          const selectedAddress = addressUser.find(
+                            (address) => address.Id === selectedId
+                          );
+                          setSelectedAddress(selectedAddress);
+                          
+                          // setDistricts(selectedAddress.district);
+                          // selectedWard(selectedAddress.ward);
+                        }}
+                      >
                       <option value="">Lựa chọn địa chỉ</option>
-                      {addresses.map((address) => (
-                        <option key={address.id} value={address.id}>
-                          {address.name}
+                      {addressUser.map((address) => (
+                        <option key={address.Id} value={address.Id}>
+                          {address.nameRecipient +
+                            " - " +
+                            address.detailAddress +
+                            " - " +
+                            address.city}
                         </option>
                       ))}
                     </select>
                     <div className="row">
                       <div className="col-lg-6 col-md-6">
                         <div className="billing-info mb-20">
-                          <label>First Name</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>Last Name</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-12">
-                        <div className="billing-info mb-20">
-                          <label>Company Name</label>
-                          <input type="text" />
+                          <label>Tên Người nhận</label>
+                          <input
+                            type="text"
+                            value={selectedAddress?.nameRecipient}
+                            onChange={(e) => handleChangeState("nameRecipient", e.target.value)}
+
+                          />
                         </div>
                       </div>
                       <div className="col-lg-12">
                         <div className="billing-select mb-20">
-                          <label>Country</label>
-                          <select>
-                            <option>Select a country</option>
-                            <option>Azerbaijan</option>
-                            <option>Bahamas</option>
-                            <option>Bahrain</option>
-                            <option>Bangladesh</option>
-                            <option>Barbados</option>
-                          </select>
+                          <label>
+                            Tỉnh: {selectedAddress.city}
+                            <select
+                              value={selectedProvince}
+                              onChange={handleProvinceChange}
+                              name=""
+                            >
+                              <option value="">Chọn Tỉnh</option>
+                              {provinces.map((province) => (
+                                <option key={province.id} value={province.code}>
+                                  {province.name}
+                                </option>
+                              ))}
+                            </select>
+                          </label>
+                          <label>
+                            Huyện: {selectedAddress.district}
+                            <select
+                              value={selectedDistrict}
+                              onChange={handleDistrictChange}
+                              name="district"
+                            >
+                              <option value="">Chọn Huyện</option>
+                              {districts.map((district) => (
+                                  <option
+                                  key={district.id}
+                                  value={district.code}
+                                  name={district.name}
+                                >
+                                  {district.name}
+                                </option>
+                              ))}
+                            </select>
+                          </label>
+                          <label>
+                            Phường: {selectedAddress.ward}
+                            <select
+                              value={selectedWard}
+                              onChange={handleWardChange}
+                            >
+                              <option value="">Chọn phường</option>
+                              {wards.map((ward) => (
+                                <option key={ward.id} value={ward.code}>
+                                  {ward.name}
+                                </option>
+                              ))}
+                            </select>
+                          </label>
                         </div>
                       </div>
                       <div className="col-lg-12">
                         <div className="billing-info mb-20">
-                          <label>Street Address</label>
+                          <label>Địa chỉ</label>
                           <input
                             className="billing-address"
-                            placeholder="House number and street name"
+                            placeholder="Số nhà tên đường"
                             type="text"
+                            value={selectedAddress?.detailAddress}
+                            onChange={(e) => handleChangeState("detailAddress", e.target.value)}
                           />
-                          <input
-                            placeholder="Apartment, suite, unit etc."
-                            type="text"
-                          />
-                        </div>
-                      </div>
-                      <div className="col-lg-12">
-                        <div className="billing-info mb-20">
-                          <label>Town / City</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>State / County</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>Postcode / ZIP</label>
-                          <input type="text" />
                         </div>
                       </div>
                       <div className="col-lg-6 col-md-6">
                         <div className="billing-info mb-20">
                           <label>Phone</label>
-                          <input type="text" />
+                          <input type="text" value={selectedAddress?.numberPhone} 
+                            onChange={(e) => handleChangeState("numberPhone", e.target.value)}
+                          />
                         </div>
                       </div>
                       <div className="col-lg-6 col-md-6">
                         <div className="billing-info mb-20">
-                          <label>Email Address</label>
-                          <input type="text" />
+                          <label>Postcode / ZIP</label>
+                          <input type="text" value={selectedAddress?.zipcode} 
+                            onChange={(e) => handleChangeState("zipcode", e.target.value)}
+                          />
                         </div>
-                      </div>
-                    </div>
-
-                    <div className="additional-info-wrap">
-                      <h4>Additional information</h4>
-                      <div className="additional-info">
-                        <label>Order notes</label>
-                        <textarea
-                          placeholder="Notes about your order, e.g. special notes for delivery. "
-                          name="message"
-                          defaultValue={""}
-                        />
                       </div>
                     </div>
                   </div>
@@ -164,13 +263,13 @@ const addresses = [
 
                 <div className="col-lg-5">
                   <div className="your-order-area">
-                    <h3>Your order</h3>
+                    <h3>Đơn hàng của bạn</h3>
                     <div className="your-order-wrap gray-bg-4">
                       <div className="your-order-product-info">
                         <div className="your-order-top">
                           <ul>
-                            <li>Product</li>
-                            <li>Total</li>
+                            <li>Sản phẩm</li>
+                            <li>Tổng</li>
                           </ul>
                         </div>
                         <div className="your-order-middle">
@@ -214,12 +313,12 @@ const addresses = [
                             })}
                           </ul>
                         </div>
-                        <div className="your-order-bottom">
+                        {/* <div className="your-order-bottom">
                           <ul>
                             <li className="your-order-shipping">Shipping</li>
                             <li>Free shipping</li>
                           </ul>
-                        </div>
+                        </div> */}
                         <div className="your-order-total">
                           <ul>
                             <li className="order-total">Total</li>
