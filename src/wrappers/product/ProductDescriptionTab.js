6cb47b486dfe1f3029efe2b73b8eac4954f3e4ff
Cozark <longho250702@gmail.com>
2023-05-03 17:14:59 +0700

    Fix big update

--- src/wrappers/product/ProductDescriptionTab.js
+++ src/wrappers/product/ProductDescriptionTab.js
@@ -1,23 +1,17 @@
-import PropTypes from "prop-types";
-import clsx from "clsx";
-import Tab from "react-bootstrap/Tab";
-import Nav from "react-bootstrap/Nav";
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import Tab from 'react-bootstrap/Tab';
+import Nav from 'react-bootstrap/Nav';
 
-const ProductDescriptionTab = ({
-  spaceBottomClass,
-  productFullDesc,
-  product,
-}) => {
+const ProductDescriptionTab = ({ spaceBottomClass, productFullDesc, product }) => {
   return (
-    <div className={clsx("description-review-area", spaceBottomClass)}>
+    <div className={clsx('description-review-area', spaceBottomClass)}>
       <div className="container">
         <div className="description-review-wrapper">
           <Tab.Container defaultActiveKey="productDescription">
             <Nav variant="pills" className="description-review-topbar">
               <Nav.Item>
-                <Nav.Link eventKey="additionalInfo">
-                  Thông tin sản phẩm
-                </Nav.Link>
+                <Nav.Link eventKey="additionalInfo">Thông tin sản phẩm</Nav.Link>
               </Nav.Item>
               <Nav.Item>
                 <Nav.Link eventKey="productDescription">Sản phẩm</Nav.Link>
@@ -52,38 +46,28 @@ const ProductDescriptionTab = ({
                         <div key={index} className="attribute-group">
                           <p>{attribute.name}:</p>
                           <ul>
-                            {attribute.attributeValues.map(
-                              (value, index) => (
-                                <li key={index}>
-                                  <span>{value.name}</span>
-                                </li>
-                              )
-                            )}
+                            {attribute.attributeValues.map((value, index) => (
+                              <li key={index}>
+                                <span>{value.name}</span>
+                              </li>
+                            ))}
                           </ul>
                         </div>
                       ) : (
-                        ""
+                        ''
                       )}
                     </>
                   ))}
                 </div>
               </Tab.Pane>
-              <Tab.Pane eventKey="productDescription">
-                {productFullDesc}
-              </Tab.Pane>
+              <Tab.Pane eventKey="productDescription">{productFullDesc}</Tab.Pane>
               <Tab.Pane eventKey="productReviews">
                 <div className="row">
                   <div className="col-lg-7">
                     <div className="review-wrapper">
                       <div className="single-review">
                         <div className="review-img">
-                          <img
-                            src={
-                              process.env.PUBLIC_URL +
-                              "/assets/img/testimonial/1.jpg"
-                            }
-                            alt=""
-                          />
+                          <img src={process.env.PUBLIC_URL + '/assets/img/testimonial/1.jpg'} alt="" />
                         </div>
                         <div className="review-content">
                           <div className="review-top-wrap">
@@ -105,23 +89,16 @@ const ProductDescriptionTab = ({
                           </div>
                           <div className="review-bottom">
                             <p>
-                              Vestibulum ante ipsum primis aucibus orci
-                              luctustrices posuere cubilia Curae Suspendisse
-                              viverra ed viverra. Mauris ullarper euismod
-                              vehicula. Phasellus quam nisi, congue id nulla.
+                              Vestibulum ante ipsum primis aucibus orci luctustrices posuere cubilia Curae
+                              Suspendisse viverra ed viverra. Mauris ullarper euismod vehicula. Phasellus quam
+                              nisi, congue id nulla.
                             </p>
                           </div>
                         </div>
                       </div>
                       <div className="single-review child-review">
                         <div className="review-img">
-                          <img
-                            src={
-                              process.env.PUBLIC_URL +
-                              "/assets/img/testimonial/2.jpg"
-                            }
-                            alt=""
-                          />
+                          <img src={process.env.PUBLIC_URL + '/assets/img/testimonial/2.jpg'} alt="" />
                         </div>
                         <div className="review-content">
                           <div className="review-top-wrap">
@@ -143,10 +120,9 @@ const ProductDescriptionTab = ({
                           </div>
                           <div className="review-bottom">
                             <p>
-                              Vestibulum ante ipsum primis aucibus orci
-                              luctustrices posuere cubilia Curae Suspendisse
-                              viverra ed viverra. Mauris ullarper euismod
-                              vehicula. Phasellus quam nisi, congue id nulla.
+                              Vestibulum ante ipsum primis aucibus orci luctustrices posuere cubilia Curae
+                              Suspendisse viverra ed viverra. Mauris ullarper euismod vehicula. Phasellus quam
+                              nisi, congue id nulla.
                             </p>
                           </div>
                         </div>
@@ -181,11 +157,7 @@ const ProductDescriptionTab = ({
                             </div>
                             <div className="col-md-12">
                               <div className="rating-form-style form-submit">
-                                <textarea
-                                  name="Your Review"
-                                  placeholder="Message"
-                                  defaultValue={""}
-                                />
+                                <textarea name="Your Review" placeholder="Message" defaultValue={''} />
                                 <input type="submit" defaultValue="Submit" />
                               </div>
                             </div>
